7e90c7a3292dcfce6a118a91a56c90d57d837802
donghyeok <tjehdgur1500@naver.com>
2022-01-28 17:22:13 +0900

    포트수정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 async function serverStarting() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
   await app.listen(3001);
 }
 serverStarting();
