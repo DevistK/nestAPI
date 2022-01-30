850403c23f310c8931078f45093e094cab185d95
donghyeok <tjehdgur1500@naver.com>
2022-01-30 19:47:24 +0900

    MODIFY : 사용하지 않는 패키지 import 삭제

--- src/movies/dto/update.movie.dto.ts
+++ src/movies/dto/update.movie.dto.ts
@@ -1,5 +1,4 @@
 import { PartialType } from "@nestjs/mapped-types";
-import { IsNumber, IsString } from "class-validator";
 import { CreateMovieDTO } from "./create-movie.dto";
 
 export class UpdateMovieDTO extends PartialType(CreateMovieDTO) { }
